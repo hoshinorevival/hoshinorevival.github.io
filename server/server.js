b00f01ff6f9d19b4a3887690cadf019936b83f29
Hammy <frideydabest@gmail.com>
2026-09-07 18:46:00 +0300

    Implement basic HTTP server with JSON response

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
+const http = require("http");
 
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+
+    res.end(JSON.stringify({
+        name: "HOSHINO",
+        version: "0.1.0",
+        status: "online",
+        message: "Welcome to HOSHINO!"
+    }));
+});
+
+server.listen(PORT, () => {
+    console.log(`HOSHINO server is running on port ${PORT}`);
+});
